626968287709e7babd75fd5abc80682dd16696b2
Leo-ui-582 <lipovetskiy88@mail.ru>
2022-03-22 18:00:19 +0300

    lesson4

---
+++ library/todo/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework.serializers import HyperlinkedModelSerializer
+from models import TodoList
+
+
+class TodoModelSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = TodoList
+        fields = '__all__'
