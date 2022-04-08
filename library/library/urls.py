2d1d4ef5a4d6286ddfe31d66119a442e48830f03
Leo-ui-582 <lipovetskiy88@mail.ru>
2022-04-08 15:12:06 +0300

    lesson9

--- library/library/urls.py
+++ library/library/urls.py
@@ -24,6 +24,17 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from userapp.views import UserListAPIView
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title='library',
+        default_version='0.1',
+        description='Docs',
+        contact=openapi.Contact('jhgfd@com'),
+        license=openapi.License(name='MIT License'),
+
+    )
+)
+
 router = DefaultRouter()
 router.register('authors', AuthorModelViewSet)
 router.register('books', BookModelViewSet)
@@ -42,19 +53,9 @@ urlpatterns = [
     re_path(r'^api/(?P<version>\d\.\d)/users/$', UserListAPIView.as_view()),
     path('api/users/0.1', include('userapp.urls', namespace='0.1')),
     path('api/users/0.2', include('userapp.urls', namespace='0.2')),
-    # re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    # path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    # path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Library",
-        default_version='0.1',
-        description="Documentation to out project",
-        contact=openapi.Contact(email="admin@admin.local"),
-        license=openapi.License(name="MIT License"),
-    ),
-    public=True,
-    permission_classes=[permissions.AllowAny],
-)
+
